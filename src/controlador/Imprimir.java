847e210e2b49f5a0053b39eed2135c48fd83bd64
VladyslavaTrubina-ll <vladyslava.trubina@elorrieta-errekamari.com>
2026-01-31 18:58:13 +0200

    anadir bienvenida en imprimir controladar

--- src/controlador/Imprimir.java
+++ src/controlador/Imprimir.java
@@ -2,11 +2,20 @@ package controlador;
 
 import java.util.ArrayList;
 
+import modelo.EspectadoresSesion;
 import modelo.FechaSesion;
 import modelo.OrarioPrecioSalaSesion;
 import modelo.Pelicula;
 
 public class Imprimir {
+	
+	public static void bienvenida(){
+		
+		System.out.println("=================================");
+		System.out.println("	  Bienvenido a cine ");
+		System.out.println("   Hace login con tu cuenta para comprar entradas");
+		System.out.println("=================================");
+	}
 
 	public static void imprimirPeliculas(ControladorDB controlador) {
 		System.out.println("pelicuals disponibles");
@@ -17,9 +26,21 @@ public class Imprimir {
 			System.out.println(p.toString());
 		}
 	}
+	public static ArrayList<EspectadoresSesion> imprimirEspectadores(ControladorDB controlador, FechaSesion fecha,
+			OrarioPrecioSalaSesion horarioElegido) {
+		ArrayList<FechaSesion> unafecha = new ArrayList<>();
+		unafecha.add(fecha);
+		ArrayList<OrarioPrecioSalaSesion> unorario = new ArrayList<>();
+		unorario.add(horarioElegido);
+		ArrayList<EspectadoresSesion> numespectadores = controlador.obtenerespectadoresporsesion(unafecha, unorario);
+		if (numespectadores.isEmpty()) {
+			System.out.println("No hay espectadores para esta sesión.");
+		}
+		return numespectadores;
+	}
 	
-	public static ArrayList<FechaSesion> mostarFecha(String titulo) {
-		ArrayList<FechaSesion> fechas = gestorCine.controlador.obtenerfechasporperli(titulo);
+	/*public static ArrayList<FechaSesion> mostarFecha(String titulo) {
+		ArrayList<FechaSesion> fechas = modelo.gestorCine.controlador.obtenerfechasporperli(titulo);
 		for (int i = 0; i < fechas.size(); i++) {
 			System.out.println((1 + i) + ". " + (fechas.get(i)));
 		}
@@ -35,5 +56,5 @@ public class Imprimir {
 			System.out.println((1 + i) + ". " + (horaPrecioSala.get(i)));
 		}
 		return horaPrecioSala;
-	}
+	}*/
 }
