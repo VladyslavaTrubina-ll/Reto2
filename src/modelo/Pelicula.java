014272d3fe5c5ed78cb7c43183738b91b8eb9fce
VladyslavaTrubina-ll <vladyslava.trubina@elorrieta-errekamari.com>
2026-02-03 09:53:42 +0200

    anadir mas atributos (como en BD)

--- src/modelo/Pelicula.java
+++ src/modelo/Pelicula.java
@@ -1,24 +1,37 @@
 package modelo;
 
 public class Pelicula {
-	private String nombre;
+	private String nombre, genero;
 	private int duracion;
+	private double precioBase;
 
-	public Pelicula(String titulo, int duracion) {
+	public Pelicula(String titulo, int duracion, String genero, double precioBase) {
 		this.nombre = titulo;
 		this.duracion = duracion;
+		this.genero = genero;
+		this.precioBase = precioBase;
+		
 	}
 
 	@Override
 	public String toString() {
-		return String.format("%s | %d min", nombre, duracion); // TODO? reformat
+		return "Pelicula [nombre=" + nombre + ", duracion=" + duracion + ", genero=" + genero + ", precioBase=" + precioBase + "]";
 	}
 
 	public String getNombre() {
 		return nombre;
 	}
-
+	
 	public int getDuracion() {
 		return duracion;
 	}
+
+	public String getGenero() {
+		return genero;
+	}
+	
+	public double getPrecioBase() {
+		return precioBase;
+	}
+
 }
